dd1aebcd051069e1a07d35c5e4318e031c0482f3
David Qu <dq4443@gmail.com>
2026-04-16 12:37:31 +1000

    Add: network-delay-time - submission-0

---
+++ Data Structures & Algorithms/network-delay-time/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # dijkstra's
+        # use minheap pq [(distance, node)]
+        # build distance map {node: distance}
+
+        # build graph
+        graph = defaultdict(list) # {node: (neigh, weight)}
+        for u, v, w in times:
+            graph[u].append((v, w))
+        print(graph)
+        
+        pq = [(0, k)]
+        dist = {}
+
+        while pq:
+            d, node = heapq.heappop(pq)
+            if node in dist:
+                continue
+            dist[node] = d
+            print(f'node: {node}')
+            print(node, graph[node])
+            for neigh, weight in graph[node]:
+                heapq.heappush(pq, (d + weight, neigh))
+
+        print(dist)
+
+        return max(dist.values()) if len(dist) == n else -1
