60fb849287e1124f843922f17fb1f1c873f2299b
David Qu <dq4443@gmail.com>
2026-04-19 21:16:34 +1000

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        # two pointer approach, keep track of palindrome count
+        if not s:
+            return 0
+        if len(s) == 1:
+            return 1
+
+        res = 0
+
+        def expand(l, r):
+            nonlocal res
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+
+                l -= 1
+                r += 1
+
+        for i in range(len(s)):
+            expand(i, i)
+            expand(i, i + 1)
+
+        return res
